4e911cde099525ec3a68db5ababd861739565b52
mupersega <cambodiam@hotmail.com>
2021-09-22 17:47:23 +1000

    dissected main into modules for utilities.rb

---
+++ classes/display.rb
@@ -0,0 +1,22 @@
+class Display
+    # Display class will prepare the user to create a world. It's main loop will first welcome the user and ask them to resize their viewport window. Ask the user to kindly set width and height on both axes.
+    
+    def initialize
+        @world_rows = 30
+        @world_columns = 30
+        @world_tile_width = 3
+        @display_width = @world_columns * @world_tile_width
+    end
+    def welcome_user
+    end
+
+    def check_viewport_width()
+    end
+
+    def check_viewport_height()
+    end
+    def main_loop
+        welcome_user
+        check_viewport_width
+    end
+end
--- main.rb
+++ main.rb
@@ -1,35 +1,38 @@
 require 'rainbow'
+require_relative 'modules/utilities'
 
-def create_random_2d_array(num_rows, num_columns, min_value, max_value)
-    r = num_rows
-    c = num_columns
-    min = min_value
-    max = max_value
-    new_array = []
-    r.times {new_array << c.times.map {rand(min..max)}}
-    return new_array
-end
+include Utilities::ArrayFuncs
 
-def pretty_print_array(arr)
-    arr.each do |line|
-        line.each do |val|
-            case val
-            when "~,~"
-                print Rainbow(val).color(:navyblue).bg(:navyblue)
-            when "~.~"
-                print Rainbow(val).blue.bg(:blue)
-            when "'.'"
-                print Rainbow(val).color(:khaki).bg(:yellow)
-            when " - "
-                print Rainbow(val).yellow.bg(:green)
-            else
-                print Rainbow(val).color(:slategray).bg(:darkslategray)
-            end
-        end
-        puts ""
-    end
-    puts "--" * 20
-end
+# def create_random_2d_array(total_rows, num_columns, min_value, max_value)
+#     r = total_rows
+#     c = num_columns
+#     min = min_value
+#     max = max_value
+#     new_array = []
+#     r.times {new_array << c.times.map {rand(min..max)}}
+#     return new_array
+# end
+
+# def pretty_print_array(arr)
+#     arr.each do |line|
+#         line.each do |val|
+#             case val
+#             when "~,~"
+#                 print Rainbow(val).color(:navyblue).bg(:navyblue)
+#             when "~.~"
+#                 print Rainbow(val).blue.bg(:blue)
+#             when "'.'"
+#                 print Rainbow(val).color(:khaki).bg(:yellow)
+#             when " - "
+#                 print Rainbow(val).yellow.bg(:green)
+#             else
+#                 print Rainbow(val).color(:slategray).bg(:darkslategray)
+#             end
+#         end
+#         puts ""
+#     end
+#     puts "--" * 20
+# end
 
 # smooth_array(arr)
     # make empty array to take lists of averages
@@ -45,8 +48,8 @@ end
 
 def smooth_array(arr, smooth_radius)
     smooth_rad = smooth_radius
-    num_rows = arr.length()
-    num_cols = arr[0].length()
+    total_rows = arr.length()
+    total_cols = arr[0].length()
     new_array = []
     # for each row
     arr.each_with_index do |row, row_index|
@@ -70,8 +73,8 @@ def smooth_array(arr, smooth_radius)
                 # for every column in that row within x range
                 x_range.each do |x_range_val|
                     # prepare vars with modulo to allow wrapping of arrays
-                    row = y_range_val % num_rows
-                    col = x_range_val % num_cols
+                    row = y_range_val % total_rows
+                    col = x_range_val % total_cols
                     surrounding_values << arr[row][col]
                 end
             end
@@ -100,12 +103,18 @@ def average(arr)
     return arr.sum / arr.size
 end
 
-rows = 100
-cols = 30
+rows = 25
+cols = 40
 min_value = 0
 max_value = 20
-looped_array = create_random_2d_array(rows, cols, min_value, max_value)
+# looped_array = create_random_2d_array(rows, cols, min_value, max_value)
 
 # smooth_array(looped_array, 2)
 
-pretty_print_array(smooth_array(looped_array, 2))
+# pretty_print_array(smooth_array(looped_array, 2))
+
+new_arr = random_2d_array(rows, cols, min_value, max_value)
+
+new_arr = smooth_2d_array(new_arr, 2)
+display_array_clean(new_arr)
+
