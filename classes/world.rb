db532ade6fa204842653d73b55a0042291e8279e
mupersega <cambodiam@hotmail.com>
2021-09-28 20:27:00 +1000

    added an example json file, changed the initial smooth to three seperate smooths rather than 1 large smooth, took away most arguments on world init.

--- classes/world.rb
+++ classes/world.rb
@@ -2,75 +2,71 @@ require_relative '../modules/utilities'
 require_relative 'tile'
 
 class World
-	include Utilities::ArrayFuncs
+  include Utilities::ArrayFuncs
 
-	attr_reader :sea_level, :height_map
-	
-	def initialize(rows, cols, min_altitude, max_altitude, sea_level)
-		@rows = rows
-		@cols = cols
-		@min_altitude = min_altitude
-		@max_altitude = max_altitude
-		@sea_level = sea_level
-		@height_map = []
-		@tiles = []
-		setup()
-	end
+  attr_reader :sea_level, :height_map
 
-	def setup
-		# build array of arrays of random values between min & max altitudes
-		@height_map = random_2d_array(@rows, @cols, @min_altitude, @max_altitude)
-		# present_height_map()
-		# smooth the initial array arg=smooth radius
-		@height_map = smooth_height_map(4)
-		# present_height_map()
-		build_tiles(@height_map)
-	end
+  def initialize(sea_level = 50, height_map = [])
+    @rows = 20
+    @cols = 40
+    @min_altitude = 0
+    @max_altitude = 100
+    @sea_level = sea_level
+    @height_map = height_map
+    @tiles = []
+    @height_map.length.positive? ? build_tiles(@height_map) : setup
+  end
 
-	def smooth_height_map(smooth_radius)
-		# run utils func on this' height map
-		@height_map = smooth_2d_array(@height_map, smooth_radius)
-	end
+  def setup
+    # build array of arrays of random values between min & max altitudes
+    @height_map = random_2d_array(@rows, @cols, @min_altitude, @max_altitude)
+    # smooth the initial array arg=smooth radius
+    3.times {
+      @height_map = smooth_height_map(2)
+    }
+    # instantiate tiles
+    build_tiles(@height_map)
+  end
 
-	def build_tiles(an_array)
-		# for every value(altitude) in every row, make a new tile at coords (x=row_index, y=col_index) at altitude.
-		an_array.each_with_index do |row, row_index|
-			row.each_with_index do |altitude, col_index|
-				@tiles.push Tile.new(self, row_index, col_index, altitude)
-			end
-		end
-	end
+  def smooth_height_map(smooth_radius)
+    # run utils func on this' height map
+    @height_map = smooth_2d_array(@height_map, smooth_radius)
+  end
 
-	def present_height_map
-		display_array_padded(@height_map, 2)
-	end
+  def build_tiles(an_array)
+    # for every value(altitude) in every row, make a new tile at coords (x=row_index, y=col_index) at altitude.
+    an_array.each_with_index do |row, row_index|
+      row.each_with_index do |altitude, col_index|
+        @tiles.push Tile.new(self, row_index, col_index, altitude)
+      end
+    end
+  end
 
-	# used primarily for debugging, show string representations of all tiles
-	def present_tiles
-		@tiles.each_with_index do |tile, i|
-			if i % @cols == 0
-				puts ""
-			end
-			print tile
-		end
-		puts ""
-	end
+  def present_height_map
+    display_array_padded(@height_map, 2)
+  end
 
-	def draw_tiles
-		@tiles.each_with_index do |tile, i|
-			if i % @cols == 0
-				puts ""
-				print"	"
-			end
-			tile.draw()
-			# sleep(0.005)
-		end
-		puts ""
-		puts ""
-	end
+  # used primarily for debugging, show string representations of all tiles
+  def present_tiles
+    @tiles.each_with_index do |tile, i|
+      puts '' if (i % @cols).zero?
+      print tile
+    end
+    puts ''
+  end
 
-	def main_loop
-		
-	end
+  def draw_tiles
+    @tiles.each_with_index do |tile, i|
+      if (i % @cols).zero?
+        puts ''
+        print "\t"
+      end
+      tile.draw
+      # sleep(0.005)
+    end
+    puts ''
+    puts ''
+  end
 
+  def main_loop; end
 end
--- main.rb
+++ main.rb
@@ -1,21 +1,14 @@
-
 require_relative 'modules/utilities'
 require_relative 'classes/world'
 
-include Utilities::ArrayFuncs
-
-rows = 19
-cols = 40
-min_value = 0
-max_value = 100
-sea_level = 50
+sea_level = 48
 
 # new_arr = random_2d_array(rows, cols, min_value, max_value)
 
 # new_arr = smooth_2d_array(new_arr, 2)
 # display_array_clean(new_arr)
 
-world = World.new(rows, cols, min_value, max_value, sea_level)
+world = World.new(sea_level)
 
 # world.present_height_map()
 world.draw_tiles()
